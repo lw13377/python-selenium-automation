c4b25423754f9abbc16d376845f1294dc77e7c9b
lw13377 <eabrahamyan1717@gmail.com>
2024-07-26 23:30:48 -0700

    Lesson 6 HW

---
+++ features/steps/main_page_steps.py
@@ -0,0 +1,43 @@
+from selenium.webdriver.common.by import By
+from behave import given, when, then
+from time import sleep
+
+
+@given('Open target main page')
+def open_target(context):
+    context.app.main_page.open()
+
+
+@when('Search for {product}')
+def search_product(context, product):
+    context.app.header.search_product()
+    print('Step layer:', product)
+    context.app.header.search_product(product)
+
+
+@when('Click on Cart icon')
+def click_cart(context):
+    context.driver.find_element(By.CSS_SELECTOR, "[data-test='@web/CartLink']").click()
+    context.app.header.click_cart()
+
+
+@then('Verify header in shown')
+def verify_header_present(context):
+    context.driver.find_element(By.CSS_SELECTOR, "[class*='utilityHeaderContainer']")
+@then('Verify header has {number} links')
+def verify_header_link_amount(context, number):
+    number = int(number)  
+    links = context.driver.find_elements(By.CSS_SELECTOR, "[id*='utilityNav']")
+    assert len(links) == number, f'Expected {number} links but got {len(links)}'
+
+
+
+@then('Verify can click every link')
+def verify_click_links(context):
+    links = context.driver.find_elements(By.CSS_SELECTOR, "[id*='utilityNav']")
+
+    for i in range(len(links)):
+        links = context.driver.find_elements(By.CSS_SELECTOR, "[id*='utilityNav']")
+        print(f'Clicking on link {links[i].text}')
+        links[i].click()
+        sleep(4)
